32663c2a4fd344e0103e46669472fc0fc99a1c29
kubamika16 <kuba.mika16@gmail.com>
2024-08-27 00:25:44 +0100

    proxy

--- src/instagram_poster.py
+++ src/instagram_poster.py
@@ -1,11 +1,34 @@
 from instagrapi import Client
 import os
 from dotenv import load_dotenv
+import requests
 
 # Load environment variables from a .env file
 load_dotenv()
 instagram_username = os.environ['INSTAGRAM_USERNAME']
 instagram_password = os.environ['INSTAGRAM_PASSWORD']
+
+# Set up the proxy
+host = 'brd.superproxy.io'
+port = 22225
+proxy_username = os.environ['PROXY_USERNAME'] 
+proxy_password = os.environ['PROXY_PASSWORD']
+
+proxy_url = f'http://{proxy_username}:{proxy_password}@{host}:{port}'
+
+
+proxies = {
+    'http': proxy_url,
+    'https': proxy_url
+}
+
+# Update the requests library's default settings to use the proxy
+requests_session = requests.Session()
+requests_session.proxies.update(proxies)
+
+# You can print the IP to confirm proxy usage
+response = requests_session.get("http://lumtest.com/myip.json")
+print("Proxy IP:", response.json())
 
 
 def post_to_instagram(photo_path, caption):
@@ -23,6 +46,10 @@ def post_to_instagram(photo_path, caption):
 
     # Initialize the Instagram client
     client = Client()
+
+    # Assign the custom requests session with the proxy to the client
+    client.private.requests = requests_session
+
     # Login to Instagram using the provided credentials
     client.login(instagram_username, instagram_password)
 
