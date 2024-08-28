d7762279f85fe937121c82296463f8389d31911d
kubamika16 <kuba.mika16@gmail.com>
2024-08-28 18:37:24 +0100

    test

--- src/instagram_poster.py
+++ src/instagram_poster.py
@@ -62,5 +62,5 @@ def post_to_instagram(photo_url, caption):
         print("Response Text:", publish_response.text)
         return False
 
-    print("Successfully posted to Instagram")
+    print("Successfully posted to Instagram. Great job!")
     return True
